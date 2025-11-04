3ce108f1ce31a46df178484a80d031906dd92e5a
elise_g_park@brown.edu <epark107@node1901.oscar.ccv.brown.edu>
2025-11-04 18:04:31 -0500

    hypo

--- examples/voronoi_shock/case.py
+++ examples/voronoi_shock/case.py
@@ -6,16 +6,15 @@ ps = 248758.567
 gam = 1.4
 rho = 1.0
 c_l = math.sqrt(1.4 * ps / rho)
-vel = 230.0
+vel = 500#230.0
 
 leng = 1.0
-Ny = 100.0
+Ny = 200.0
 Nx = Ny * 3
 dx = leng / Nx
 
 time_end = 5 * leng / vel
 cfl = 0.1
-
 dt = cfl * dx / c_l
 Nt = int(time_end / dt)
 
@@ -35,8 +34,8 @@ print(
             "p": 0,
             "dt": dt,
             "t_step_start": 0,
-            "t_step_stop": 10000, #1000
-            "t_step_save": 200,#10
+            "t_step_stop": 10000,
+            "t_step_save": 200,
             # Simulation Algorithm Parameters
             "num_patches": 4,
             "model_eqns": 2,
@@ -52,7 +51,8 @@ print(
             "mapped_weno": "T",
             "null_weights": "F",
             "mp_weno": "F",
-            "riemann_solver": 2,
+            "riemann_solver": 1, #2
+            "fd_order": 4,
             "wave_speeds": 1,
             "avg_state": 2,
             "bc_x%beg": -6,
@@ -64,16 +64,18 @@ print(
             "precision": 2,
             "prim_vars_wrt": "T",
             "parallel_io": "T",
+            # Turning on hypoelasticity
+            "hypoelasticity": "T",
             # Patch 1: Background
             "patch_icpp(1)%geometry": 3,
             "patch_icpp(1)%x_centroid": 0.0,
             "patch_icpp(1)%y_centroid": 0.0,
             "patch_icpp(1)%length_x": 10 * leng,
             "patch_icpp(1)%length_y": leng,
-            "patch_icpp(1)%vel(1)": vel,
+            "patch_icpp(1)%vel(1)": 0,
             "patch_icpp(1)%vel(2)": 0.0e00,
             "patch_icpp(1)%pres": 101325.0,
-            "patch_icpp(1)%alpha_rho(1)": 1.29,#water
+            "patch_icpp(1)%alpha_rho(1)": 1.29,
             "patch_icpp(1)%alpha_rho(2)": 0.0e00,
             "patch_icpp(1)%alpha(1)": 1.0e00,
             "patch_icpp(1)%alpha(2)": 0.0e00,
@@ -87,17 +89,17 @@ print(
             "patch_icpp(2)%vel(1)": vel,
             "patch_icpp(2)%vel(2)": 0.0e00,
             "patch_icpp(2)%pres": ps,
-            "patch_icpp(2)%alpha_rho(1)": 2.4, #shocked water
+            "patch_icpp(2)%alpha_rho(1)": 2.4,
             "patch_icpp(2)%alpha_rho(2)": 0.0e00,
             "patch_icpp(2)%alpha(1)": 1.0e00,
             "patch_icpp(2)%alpha(2)": 0.0e00,
-            # Patch 3: Bubble
+            # Patch 3: Lung
             "patch_icpp(3)%geometry": 21,
             "patch_icpp(3)%model_filepath": "voro_flat.stl",
-            "patch_icpp(3)%model_spc": 100,
-            "patch_icpp(3)%model_scale(1)": 4,
-            "patch_icpp(3)%model_scale(2)": 4, 
-            "patch_icpp(3)%model_translate(1)":0.25,#0.25
+            "patch_icpp(3)%model_spc": 10,
+            "patch_icpp(3)%model_scale(1)": 8,
+            "patch_icpp(3)%model_scale(2)": 8,
+            "patch_icpp(3)%model_translate(1)":1.50,
             "patch_icpp(3)%model_translate(2)":-0.25,   
             "patch_icpp(3)%model_threshold": 0.99,
             "patch_icpp(3)%x_centroid": 0.0e00,
@@ -105,23 +107,23 @@ print(
             "patch_icpp(3)%alter_patch(1)": "T",
             "patch_icpp(3)%vel(1)": 0.0,
             "patch_icpp(3)%vel(2)": 0.0e00,
-            "patch_icpp(3)%pres": 101325.0,
-            "patch_icpp(3)%alpha_rho(1)": 1.29,#water
+            "patch_icpp(3)%pres": 101325.00,
+            "patch_icpp(3)%alpha_rho(1)": 1.29,
             "patch_icpp(3)%alpha_rho(2)": 0.0,
             "patch_icpp(3)%alpha(1)": 1.0e00,
             "patch_icpp(3)%alpha(2)": 0.0e00,
-            # Patch 4: Gas Behind STL
+            #Gas Background
             "patch_icpp(4)%geometry": 3,
             "patch_icpp(4)%alter_patch(1)": "T",
-            "patch_icpp(4)%x_centroid": 0.0,
+            "patch_icpp(4)%x_centroid": 1.25,
             "patch_icpp(4)%y_centroid": 0.0,
-            "patch_icpp(4)%length_x": 10*leng,
+            "patch_icpp(4)%length_x":2.5* leng,
             "patch_icpp(4)%length_y": leng,
             "patch_icpp(4)%vel(1)": 0.0,
             "patch_icpp(4)%vel(2)": 0.0,
             "patch_icpp(4)%pres": 101325.0,
             "patch_icpp(4)%alpha_rho(1)": 0.0,
-            "patch_icpp(4)%alpha_rho(2)": 0.167, #gas
+            "patch_icpp(4)%alpha_rho(2)": .167, #gas
             "patch_icpp(4)%alpha(1)": 0.0,
             "patch_icpp(4)%alpha(2)": 1.0,
             # Fluids Physical Parameters
