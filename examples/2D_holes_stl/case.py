3c9b6ff5ea13b0cd16ec8f334ecb7cd66ffa63ac
elise_g_park@brown.edu <epark107@node1913.oscar.ccv.brown.edu>
2025-12-09 10:33:25 -0500

    working with 2D_voronoi_stl

---
+++ examples/2D_holes_stl/case.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+import math
+import json
+
+pi = 3.141592653589
+patmos = 101325.0
+
+# gas parameters
+gammag = 1.4  # gas
+Bg = 0.0  # Pa
+rhog = 1.18  # kg/m^3
+c_g = 347.2  # m/s
+G_g = 0.0  # Pa
+
+# liquid parameters
+gammal = 5.5
+Bl = 0 * 492.0e6  # Pa
+rhol = 996.0  # kg/m^3
+G_l = 5.0e3  # Pa
+Cv_l = 1816.0
+
+# shock parameters
+lambda_wave = 200.0e-6
+delta_P_s = 200000.0
+P_amp = delta_P_s + patmos
+
+c_l = math.sqrt(gammal * (patmos + Bl) / rhol)
+
+g_r = gammal
+g_l = gammal
+p_r = patmos
+p_l = P_amp
+B_r = Bl
+B_l = Bl
+r_r = rhol
+c_r = c_l
+
+rho_shock_l = ((((g_r + 1) / (g_r - 1)) * ((p_l + B_r) / (p_r + B_r)) + 1) / (((g_r + 1) / (g_r - 1)) + (p_l + B_r) / (p_r + B_r))) * r_r
+
+ushock_l = (c_r / g_r) * ((p_l / p_r) - 1) * (p_r / (p_r + B_r)) / math.sqrt(((g_r + 1) / (2 * g_r)) * ((p_l / p_r) - 1) * (p_r / (p_r + B_r)) + 1)
+
+ps = 248758.567
+gam = 1.4
+rho = 1.0
+c_l_for_dt = math.sqrt(1.4 * ps / rho)
+vel = 500.0
+
+leng = 1.0
+Ny = 200.0
+Nx = Ny * 3.0
+dx = leng / Nx
+eps = 1e-3
+
+time_end = 5.0 * leng / vel
+cfl = 0.02  # 0.1
+dt = cfl * dx / c_l_for_dt
+Nt = int(time_end / dt)
+
+print(
+    json.dumps(
+        {
+            # Logistics
+            "run_time_info": "T",
+            "x_domain%beg": -leng / 2.0,
+            "x_domain%end": leng / 2 + 2.0 * leng,
+            "y_domain%beg": -leng / 2.0,
+            "y_domain%end": leng / 2.0,
+            "m": int(Nx),
+            "n": int(Ny),
+            "p": 0,
+            "dt": dt,
+            "t_step_start": 0,
+            "t_step_stop": 10000,
+            "t_step_save": 100,
+            # simulation algorithm parameters
+            "num_patches": 2,
+            "model_eqns": 2,
+            "alt_soundspeed": "F",
+            "num_fluids": 2,
+            "mpp_lim": "F",
+            "mixture_err": "T",
+            "time_stepper": 3,
+            "weno_order": 5,
+            "weno_eps": 1.0e-16,
+            "weno_Re_flux": "F",
+            "weno_avg": "F",
+            "mapped_weno": "T",
+            "null_weights": "F",
+            "mp_weno": "T",
+            "riemann_solver": 1,
+            "wave_speeds": 1,
+            "avg_state": 2,
+            "fd_order": 1,
+            "bc_x%beg": -2,
+            "bc_x%end": -2,
+            "bc_y%beg": -2,
+            "bc_y%end": -2,
+            "format": 1,
+            "precision": 2,
+            "prim_vars_wrt": "T",
+            "parallel_io": "T",
+            # turning on hypoelasticity
+            # "hypoelasticity": "T",
+            # patch 1: Gas Background
+            "patch_icpp(1)%geometry": 3,
+            "patch_icpp(1)%x_centroid": 1.25,
+            "patch_icpp(1)%y_centroid": 0.0,
+            "patch_icpp(1)%length_x": 10 * leng,
+            "patch_icpp(1)%length_y": leng,
+            "patch_icpp(1)%vel(1)": 0.0,
+            "patch_icpp(1)%vel(2)": 0.0,
+            "patch_icpp(1)%pres": patmos,
+            "patch_icpp(1)%alpha_rho(1)": rhol * eps,
+            "patch_icpp(1)%alpha_rho(2)": rhog * (1.0 - eps),  # gas
+            "patch_icpp(1)%alpha(1)": eps,
+            "patch_icpp(1)%alpha(2)": 1.0 - eps,
+            # patch 2: shock
+            "patch_icpp(2)%geometry": 3,
+            "patch_icpp(2)%alter_patch(1)": "T",
+            "patch_icpp(2)%x_centroid": -3.0 * leng / 8.0,
+            "patch_icpp(2)%y_centroid": 0.0,
+            "patch_icpp(2)%length_x": leng / 4.0,
+            "patch_icpp(2)%length_y": leng,
+            "patch_icpp(2)%vel(1)": ushock_l,
+            "patch_icpp(2)%vel(2)": 0.0e00,
+            "patch_icpp(2)%pres": P_amp,
+            "patch_icpp(2)%alpha_rho(1)": rho_shock_l * (1.0 - eps),
+            "patch_icpp(2)%alpha_rho(2)": rhog * eps,
+            "patch_icpp(2)%alpha(1)": 1.0e00 - eps,
+            "patch_icpp(2)%alpha(2)": eps,
+             # Fluids Physical Parameters
+            "fluid_pp(1)%gamma": 1.0 / (gammal - 1.0),
+            "fluid_pp(1)%pi_inf": gammal * Bl / (gammal - 1.0),
+            "fluid_pp(1)%G": G_l,
+            "fluid_pp(2)%gamma": 1.0 / (gammag - 1.0),
+            "fluid_pp(2)%pi_inf": gammag * Bg / (gammag - 1.0),
+            "fluid_pp(2)%G": G_g,
+        }
+    )
+)
--- examples/2D_voronoi_stl/case.py
+++ examples/2D_voronoi_stl/case.py
@@ -2,82 +2,49 @@
 import math
 import json
 
-pi = 3.141592653589
-patmos = 101325.0
-
-# gas parameters
-gammag = 1.4  # gas
-Bg = 0.0  # Pa
-rhog = 1.18  # kg/m^3
-c_g = 347.2  # m/s
-G_g = 0.0  # Pa
-
-# liquid parameters
-gammal = 5.5
-Bl = 0 * 492.0e6  # Pa
-rhol = 996.0  # kg/m^3
-G_l = 5.0e3  # Pa
-Cv_l = 1816.0
-
-# shock parameters
-lambda_wave = 200.0e-6
-delta_P_s = 200000.0
-P_amp = delta_P_s + patmos
-
-c_l = math.sqrt(gammal * (patmos + Bl) / rhol)
-
-g_r = gammal
-g_l = gammal
-p_r = patmos
-p_l = P_amp
-B_r = Bl
-B_l = Bl
-r_r = rhol
-c_r = c_l
-
-rho_shock_l = ((((g_r + 1) / (g_r - 1)) * ((p_l + B_r) / (p_r + B_r)) + 1) / (((g_r + 1) / (g_r - 1)) + (p_l + B_r) / (p_r + B_r))) * r_r
-
-ushock_l = (c_r / g_r) * ((p_l / p_r) - 1) * (p_r / (p_r + B_r)) / math.sqrt(((g_r + 1) / (2 * g_r)) * ((p_l / p_r) - 1) * (p_r / (p_r + B_r)) + 1)
-
 ps = 248758.567
-gam = 1.4
+gammal = 6.68
+gammag = 1.4
 rho = 1.0
-c_l_for_dt = math.sqrt(1.4 * ps / rho)
-vel = 500.0
+c_l = math.sqrt(1.4 * ps / rho)
+vel = 500
+Bl = 4.05e3 #4.05e8
+Bg = 0.0
 
 leng = 1.0
 Ny = 200.0
-Nx = Ny * 3.0
+Nx = Ny * 3
 dx = leng / Nx
-eps = 1e-3
 
-time_end = 5.0 * leng / vel
-cfl = 0.02  # 0.1
-dt = cfl * dx / c_l_for_dt
+time_end = 5 * leng / vel
+cfl = 0.1
+dt = cfl * dx / c_l
 Nt = int(time_end / dt)
 
+# Configuring case dictionary
 print(
     json.dumps(
         {
             # Logistics
             "run_time_info": "T",
+            # Computational Domain Parameters
             "x_domain%beg": -leng / 2.0,
-            "x_domain%end": leng / 2 + 2.0 * leng,
+            "x_domain%end": leng / 2 + 2 * leng,
             "y_domain%beg": -leng / 2.0,
             "y_domain%end": leng / 2.0,
             "m": int(Nx),
             "n": int(Ny),
             "p": 0,
             "dt": dt,
-            "t_step_start": 200,
-            "t_step_stop": 5000,
-            "t_step_save": 200,
-            # simulation algorithm parameters
-            "num_patches": 3,
+            "t_step_start": 0,
+            "t_step_stop": 10000,
+            "t_step_save": 100,
+            # Simulation Algorithm Parameters
+            "num_patches": 4,
             "model_eqns": 2,
             "alt_soundspeed": "F",
             "num_fluids": 2,
-            "mpp_lim": "F",
+            "mpp_lim": "T",
             "mixture_err": "T",
             "time_stepper": 3,
             "weno_order": 5,
@@ -86,78 +53,84 @@ print(
             "weno_avg": "F",
             "mapped_weno": "T",
             "null_weights": "F",
-            "mp_weno": "T",
-            "riemann_solver": 1,
+            "mp_weno": "F",
+            "riemann_solver": 2,
             "wave_speeds": 1,
             "avg_state": 2,
-            "fd_order": 1,
-            "bc_x%beg": -2,
-            "bc_x%end": -2,
-            "bc_y%beg": -2,
-            "bc_y%end": -2,
+            "bc_x%beg": -6,
+            "bc_x%end": -6,
+            "bc_y%beg": -6,
+            "bc_y%end": -6,
+            # Formatted Database Files Structure Parameters
             "format": 1,
             "precision": 2,
             "prim_vars_wrt": "T",
             "parallel_io": "T",
-            # turning on hypoelasticity
-            # "hypoelasticity": "T",
-            # patch 1: Gas Background
+            # Patch 1: Background
             "patch_icpp(1)%geometry": 3,
-            "patch_icpp(1)%x_centroid": 1.25,
+            "patch_icpp(1)%x_centroid": 0.0,
             "patch_icpp(1)%y_centroid": 0.0,
             "patch_icpp(1)%length_x": 10 * leng,
             "patch_icpp(1)%length_y": leng,
-            "patch_icpp(1)%vel(1)": 0.0,
-            "patch_icpp(1)%vel(2)": 0.0,
-            "patch_icpp(1)%pres": patmos,
-            "patch_icpp(1)%alpha_rho(1)": rhol * eps,
-            "patch_icpp(1)%alpha_rho(2)": rhog * (1.0 - eps),  # gas
-            "patch_icpp(1)%alpha(1)": eps,
-            "patch_icpp(1)%alpha(2)": 1.0 - eps,
-            # patch 2: shock
+            "patch_icpp(1)%vel(1)": 0,
+            "patch_icpp(1)%vel(2)": 0.0e00,
+            "patch_icpp(1)%pres": 101325.0,
+            "patch_icpp(1)%alpha_rho(1)": 1.29,
+            "patch_icpp(1)%alpha_rho(2)": 0.0e00,
+            "patch_icpp(1)%alpha(1)": 1.0e00,
+            "patch_icpp(1)%alpha(2)": 0.0e00,
+            # Patch 2: Shocked state
             "patch_icpp(2)%geometry": 3,
             "patch_icpp(2)%alter_patch(1)": "T",
-            "patch_icpp(2)%x_centroid": -3.0 * leng / 8.0,
+            "patch_icpp(2)%x_centroid": -3 * leng / 8.0,
             "patch_icpp(2)%y_centroid": 0.0,
             "patch_icpp(2)%length_x": leng / 4.0,
             "patch_icpp(2)%length_y": leng,
-            "patch_icpp(2)%vel(1)": ushock_l,
+            "patch_icpp(2)%vel(1)": vel,
             "patch_icpp(2)%vel(2)": 0.0e00,
-            "patch_icpp(2)%pres": P_amp,
-            "patch_icpp(2)%alpha_rho(1)": rho_shock_l * (1.0 - eps),
-            "patch_icpp(2)%alpha_rho(2)": rhog * eps,
-            "patch_icpp(2)%alpha(1)": 1.0e00 - eps,
-            "patch_icpp(2)%alpha(2)": eps,
+            "patch_icpp(2)%pres": ps,
+            "patch_icpp(2)%alpha_rho(1)": 2.4,
+            "patch_icpp(2)%alpha_rho(2)": 0.0e00,
+            "patch_icpp(2)%alpha(1)": 1.0e00,
+            "patch_icpp(2)%alpha(2)": 0.0e00,
             # Patch 3: Lung
             "patch_icpp(3)%geometry": 21,
-            "patch_icpp(3)%model_filepath": "voro_flat.stl",
+            "patch_icpp(3)%model_filepath": "holes_flat_fusion_remesh.stl",
             "patch_icpp(3)%model_spc": 10,
-            "patch_icpp(3)%model_scale(1)": 8,
-            "patch_icpp(3)%model_scale(2)": 8,
-            "patch_icpp(3)%model_translate(1)": 1.25,
-            "patch_icpp(3)%model_translate(2)": -0.25,
-            "patch_icpp(3)%model_threshold": 0.99999999,
+            "patch_icpp(3)%model_scale(1)": 0.25,
+            "patch_icpp(3)%model_scale(2)": 0.25,
+            "patch_icpp(3)%model_translate(1)": 1.05,
+            "patch_icpp(3)%model_translate(2)":0,   
+            "patch_icpp(3)%model_threshold": 0.99,
             "patch_icpp(3)%x_centroid": 0.0e00,
             "patch_icpp(3)%y_centroid": 0.0e00,
             "patch_icpp(3)%alter_patch(1)": "T",
-            "patch_icpp(3)%x_centroid": 1.25,
-            "patch_icpp(3)%y_centroid": 0.0,
-            "patch_icpp(3)%length_x": 2.5 * leng,
-            "patch_icpp(3)%length_y": leng,
             "patch_icpp(3)%vel(1)": 0.0,
-            "patch_icpp(3)%vel(2)": 0.0,
-            "patch_icpp(3)%pres": patmos,
-            "patch_icpp(3)%alpha_rho(1)": rhol * (1.0 - eps),
-            "patch_icpp(3)%alpha_rho(2)": rhog * eps,
-            "patch_icpp(3)%alpha(1)": 1.0e00 - eps,
-            "patch_icpp(3)%alpha(2)": eps,
+            "patch_icpp(3)%vel(2)": 0.0e00,
+            "patch_icpp(3)%pres": 101325.00,
+            "patch_icpp(3)%alpha_rho(1)": 1.29, #1.29,
+            "patch_icpp(3)%alpha_rho(2)": 0.0, #0.0,
+            "patch_icpp(3)%alpha(1)": 1.0, #1.0,
+            "patch_icpp(3)%alpha(2)": 0.0, #0.0
+            #Gas Background
+            "patch_icpp(4)%geometry": 3,
+            "patch_icpp(4)%alter_patch(1)": "T",
+            "patch_icpp(4)%x_centroid": 1.25,
+            "patch_icpp(4)%y_centroid": 0.0,
+            "patch_icpp(4)%length_x":2.5* leng,
+            "patch_icpp(4)%length_y": leng,
+            "patch_icpp(4)%vel(1)": 0.0,
+            "patch_icpp(4)%vel(2)": 0.0,
+            "patch_icpp(4)%pres": 101325.0,
+            "patch_icpp(4)%alpha_rho(1)": 0.0, #0.0,
+            "patch_icpp(4)%alpha_rho(2)": 0.167, #.167, #gas
+            "patch_icpp(4)%alpha(1)": 0.0, #0.0,
+            "patch_icpp(4)%alpha(2)": 1.0, #1.0
             # Fluids Physical Parameters
-            "fluid_pp(1)%gamma": 1.0 / (gammal - 1.0),
-            "fluid_pp(1)%pi_inf": gammal * Bl / (gammal - 1.0),
-            "fluid_pp(1)%G": G_l,
-            "fluid_pp(2)%gamma": 1.0 / (gammag - 1.0),
-            "fluid_pp(2)%pi_inf": gammag * Bg / (gammag - 1.0),
-            "fluid_pp(2)%G": G_g,
+            "fluid_pp(1)%gamma": 1.0 / (gammal - 1.0), #1.0e00 / (1.4e00 - 1.0e00),
+            "fluid_pp(1)%pi_inf": gammal * Bl / (gammal - 1.0), #0.0,
+            "fluid_pp(2)%gamma": 1.0 / (gammag - 1.0), #1.0e00 / (1.6666e00 - 1.0e00),
+            "fluid_pp(2)%pi_inf": gammag * Bg / (gammag - 1.0), #0.0e00,
         }
     )
 )
